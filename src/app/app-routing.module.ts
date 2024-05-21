880b4d0b81d89246c498d91ad6111a4c8e9c9972
DiegoDFB <difrancisblanco@gmail.com>
2024-05-21 14:41:34 -0300

    2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListarProdutosComponent } from './produtos/listar-produtos/listar-produtos.component';
+import { CadastrarProdutoComponent } from './produtos/cadastrar-produto/cadastrar-produto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path: 'produtos', component: ListarProdutosComponent},
+  {path: 'produto/cadastrar', component: CadastrarProdutoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
